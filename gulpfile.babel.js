64df6b039d530f5be0847202788e0671d22f7c4e
José Luis Quintana <joseluisquintana20@gmail.com>
2015-10-27 21:35:30 -0500

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,62 @@
+import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import rename from 'gulp-rename'
+import header from 'gulp-header'
+import sass from 'gulp-sass'
+import watch from 'gulp-watch'
+import minify from 'gulp-minify-css'
+import livereload from 'gulp-livereload'
+import pkg from './package.json'
+
+ // Template for banner
+const banner = `/*! ${pkg.name.charAt(0).toUpperCase()}${pkg.name.slice(1)} v${pkg.version} | ${pkg.license} | (c) ${new Date().getFullYear()} ${pkg.author.name} */
+`
+
+// Paths
+const paths = {
+  src: 'src/{,*/}*.{scss,sass}',
+  dest: 'dist/'
+}
+
+// Process and minify Sass files
+gulp.task('build:styles', () => {
+  return gulp.src(paths.src)
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: false
+    }))
+    .pipe(gulp.dest(paths.dest))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest(paths.dest))
+})
+
+// Spin up livereload server and listen for file changes
+gulp.task('listen', () => {
+  livereload.listen()
+  gulp.watch(paths.src).on('change', (file) => {
+    gulp.start('refresh')
+  })
+})
+
+// Run livereload after file change
+gulp.task('refresh', ['compile'], () => {
+  livereload.changed()
+})
+
+// Compile task
+gulp.task('compile', [
+  'build:styles'
+])
+
+// Watch task
+gulp.task('watch', [
+  'build:styles',
+  'listen'
+])
---
+++ readme.md
@@ -0,0 +1,53 @@
+# Gutr CSS
+
+> Hackable gutters for your box model.
+
+## Usage
+
+### Markup
+
+```html
+<div class="row">
+  <div class="col-md-12 gutr-pad-y1">
+    gutr-pad y 1
+          │  │ └── Width: 1em
+          │  └──── Direction: y (top, bottom)
+          └─────── Type: padding
+
+    <div class="my-wrapper gutr-mar2">
+      gutr-mar 2
+            │  └── Width: 2em
+            └───── Type: margin
+
+            Direction: x,y (default)
+    </div>
+
+  </div>
+</div>
+```
+### Settings
+
+Sass:
+
+```scss
+// Sass settings
+$gutr-unit: 1 !default;
+$gutr-limit: 6 !default;
+$gutr-measure: 'em' !default;
+$gutr-cords: ('x', 'y') !default;
+```
+
+## API
+For more helper classes check out [gutr.css](dist/gutr.css) file.
+
+## Contribution
+If you would like to contribute [pull requests](https://github.com/joseluisq/gutr-css/pulls) and [issues](https://github.com/joseluisq/gutr-css/issues) will be welcome!
+Feature requests are welcome. Please before sending some feature requests make sure provide as much detail and context as possible.
+
+## History
+Check out [releases changelog](https://github.com/joseluisq/gutr-css/releases).
+
+## License
+MIT license
+
+© 2015 [José Luis Quintana](http://quintana.io)
